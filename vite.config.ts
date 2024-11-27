7212aab564c6249b8d6a157f2678945ce8a28e66
TobiasOlofsson88 <53941698+TobiasOlofsson88@users.noreply.github.com>
2024-11-27 18:51:03 +0100

    Use npx instead of ts-node-dev

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom', // Simulate the browser environment for tests
+//     setupFiles: './vitest.setup.ts', // Optional setup file for globals like jest-dom
+//   },
+// });
