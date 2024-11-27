312cee0b6133f7e6f03527bb0f92f37eefb1233c
TobiasOlofsson88 <53941698+TobiasOlofsson88@users.noreply.github.com>
2024-11-27 16:46:20 +0100

    dev

---
+++ backend/index.ts
@@ -0,0 +1,11 @@
+export { httpConnection } from "./connection/httpConnection";
+
+export { Feature } from "./feature/feature";
+
+export * from "./interface/feature";
+export * from "./interface/httpConnection";
+export * from "./interface/koa";
+
+export { Loader } from "./loader/loader";
+
+export { createRequestLoggerMiddleware } from "./middleware/requestLogger";
--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
 export * from "./frontend/index";
+export * from "./backend/index";
