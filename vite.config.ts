b88bc851936683f8d2ea8e0bdbf04a9bfcc19495
TobiasOlofsson88 <53941698+TobiasOlofsson88@users.noreply.github.com>
2024-11-27 19:01:00 +0100

    Add vite and vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-// import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { configDefaults } from "vitest/config"; //This is required to import for defineConfig eventho it's not used
 
-// export default defineConfig({
-//   plugins: [vue()],
-//   test: {
-//     globals: true,
-//     environment: 'jsdom', // Simulate the browser environment for tests
-//     setupFiles: './vitest.setup.ts', // Optional setup file for globals like jest-dom
-//   },
-// });
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+  },
+});
