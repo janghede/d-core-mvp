875d3ed3f1d70348cdeaf191960f1874a6b9f39f
TobiasOlofsson88 <53941698+TobiasOlofsson88@users.noreply.github.com>
2024-11-29 12:03:19 +0100

    dev

--- backend/interface/koa.ts
+++ backend/interface/koa.ts
@@ -1,16 +1,13 @@
 import { DefaultContext, DefaultState } from "koa";
 import { IHttpConnection } from "./httpConnection.js";
-
-export interface IStateUser {
-  username: string;
-}
+import { IUser } from "@shared/interface/user.js";
 
 export interface IContextConnection {
   http: { [name: string]: IHttpConnection };
 }
 
 export interface ICoreState extends DefaultState {
-  user?: IStateUser;
+  user?: IUser;
   requestId?: string;
 }
 
